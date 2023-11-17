c4247baa4a5f4eef3794d8e8714106869ca0ab3e
jewel274 <jewelbrown2004@gmail.com>
2023-11-17 17:04:05 -0500

    Commit

--- app/src/main/java/com/example/uccitmobileapp/CoursesActivity.kt
+++ app/src/main/java/com/example/uccitmobileapp/CoursesActivity.kt
@@ -1,4 +1,13 @@
 package com.example.uccitmobileapp
 
+
+
+
+import android.content.Intent
+import android.net.Uri
+import android.os.Bundle
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
 class CoursesActivity {
 }
--- app/src/main/java/com/example/uccitmobileapp/FacultyStaffActivity.kt
+++ app/src/main/java/com/example/uccitmobileapp/FacultyStaffActivity.kt
@@ -1,15 +1,33 @@
 package com.example.uccitmobileapp
 
 import android.content.Intent
-import android.net.Uri
 import android.os.Bundle
+import android.widget.ListView
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import android.widget.ArrayAdapter
 
 class FacultyStaffActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_faculty_staff)
+
+        val staffList: Array<String> = resources.getStringArray(R.array.Staff)
+        val adapter = ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, staffList)
+
+        val listView: ListView = findViewById(R.id.lvStaff)
+        listView.adapter = adapter
+
+        listView.setOnItemClickListener { parent, view, position, id ->
+            Toast.makeText(this, "Clicked item : " + position, Toast.LENGTH_SHORT).show()
+            val intent = Intent(this, Details::class.java);
+            intent.putExtra("listIndex", position.toString());
+
+            this.startActivity(intent);
+        }
     }
+
+
 }
 
